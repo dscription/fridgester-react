bc7c503ad63b46ef333bebecc03652ff63902cce
dscription <dscriptionplayground@gmail.com>
2020-11-01 15:56:09 -0500

    create styled component container and add to Fridge Page.

--- src/pages/Fridge/Fridge.jsx
+++ src/pages/Fridge/Fridge.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import Container from "../../styledComponents/Container";
 
 const Fridge = () => {
-  return ( <h1>This is your fridge.</h1> );
-}
- 
+  return (
+    <>
+      <Container>
+        <h1>This is your fridge.</h1>
+        <Form>
+          <FormGroup>
+            <Label for="search">Search</Label>
+            <Input id="search" placeholder="Search for item."></Input>
+          </FormGroup>
+        </Form>
+        {/* Search Results Map */}
+      </Container>
+    </>
+  );
+};
+
 export default Fridge;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Landing = () => {
+
+  // Header IMG
+  // Header Text
+  // Signup/ Login CTA
   return ( <h1>This is the landing page.</h1> );
 }
  
