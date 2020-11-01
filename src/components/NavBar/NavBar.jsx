6adb6d95a66a4e9619d5afeca1d7db78fde3c3e1
dscription <dscriptionplayground@gmail.com>
2020-11-01 12:59:14 -0500

    Install ReactStrap, setup NavBar, add page folders

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,107 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
+import "./NavBar.css";
 
 const NavBar = ({ user, handleLogout }) => {
-    return (
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
     <>
-      {user ?
-        <nav>
-          <div className="nav-wrapper">
-            <ul id="nav-mobile" className="right">
-              <li><a href=" " className="nav-link">Welcome, {user.name}</a></li>
-              <li><a href="/users" className="nav-link">Users</a></li>
-              <li><a href=" " className="nav-link" onClick={handleLogout}>Log Out</a></li>
-            </ul>
-          </div>
-        </nav>
-      :
-        <nav>
-          <div className="nav-wrapper">
-            <ul id="nav-mobile" className="right">
-              <li><a href="/login" className="nav-link">Log In</a></li>
-              <li><a href="/users" className="nav-link">Users</a></li>
-              <li><a href="/signup" className="nav-link">Sign Up</a></li>
-            </ul>
-          </div>
-        </nav>
-      }
+      {user ? (
+        // If user is logged in they will see their avatar
+        <NavBar color="light" light expand="md">
+          {/* <NavbarBrand href="/">reactstrap</NavbarBrand> */}
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            // Take me Home/Fridge
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink id="navItem" href="/">
+                  Welcome, {user.name}
+                </NavLink>
+              </NavItem>
+              // Take me to shopping list
+              <NavItem>
+                <NavLink id="navItem" href="/shopping-list">
+                  Shopping List
+                </NavLink>
+              </NavItem>
+              // Take me to favorite recipes
+              <NavItem>
+                <NavLink id="navItem" href="/favorites">
+                  Favorite Recipes
+                </NavLink>
+              </NavItem>
+              // Let me logout
+              <NavItem>
+                <NavLink id="navItem" href=" " onClick={handleLogout}>
+                  Log Out
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText>
+              {/* this will be replaced with a real user image */}
+              <Link
+                to={{
+                  pathname: "/user",
+                  state: { user },
+                }}
+              >
+                <img
+                  src={
+                    user.avatar ? user.avatar : "https://picsum.photos/50/50"
+                  }
+                  id="user-avatar"
+                  alt=""
+                />
+              </Link>
+            </NavbarText>
+          </Collapse>
+        </NavBar>
+      ) : (
+        <Navbar color="light" light expand="md">
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/login">Log In</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/users">Users</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/signup">Signup</NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText>
+              {/* this will be replaced with a real user image */}
+              <Link
+                to={{
+                  pathname: "/login",
+                }}
+              >
+                <img
+                  src="https://picsum.photos/50/50"
+                  id="user-avatar"
+                  alt=""
+                />
+              </Link>
+            </NavbarText>
+          </Collapse>
+        </Navbar>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default NavBar;
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,7 +6,6 @@ import Login from "../Login/Login";
 import authService from "../../services/authService";
 import Users from "../Users/Users";
 import "./App.css";
-import Home from "../Home/Home";
 
 class App extends Component {
   state = {
@@ -23,42 +22,45 @@ class App extends Component {
   };
 
   render() {
-    const {user} = this.state
+    const { user } = this.state;
     return (
       <>
         <NavBar user={user} handleLogout={this.handleLogout} />
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <Home />
-          )}
-        />
-        <Route
-          exact
-          path="/signup"
-          render={({ history }) => (
-            <Signup
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/login"
-          render={({ history }) => (
-            <Login
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/users"
-          render={() => (user ? <Users /> : <Redirect to="/login" />)}
-        />
+        <div id="container-main">
+          <Route
+            exact
+            path="/"
+            render={() => (
+              // If user logged in render Fridge page, if not logged in render landing page.
+              <h1>Landing Page</h1>
+            )}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={({ history }) => (
+              <Signup
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/login"
+            render={({ history }) => (
+              <Login
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/users"
+            render={() => (user ? <Users /> : <Redirect to="/login" />)}
+          />
+        </div>
       </>
     );
   }
