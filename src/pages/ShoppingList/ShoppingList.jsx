12df42efd33aab5df3f309c22320daf7c6f72f4c
dscription <dscriptionplayground@gmail.com>
2020-11-18 12:29:24 -0500

    add styled Container to shopping list page

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Input } from 'reactstrap';
+import styled from 'styled-components';
 
 const ShoppingList = ({ location }) => {
   return (
     <>
-      <h1>Shopping List</h1>
-      <ul>
-        {location.state.ingredients.map((ingredient) => (
-          <li>
-            <Input type="checkbox" aria-label="checkbix for ingredient" />
-            {ingredient}
-          </li>
-        ))}
-      </ul>
+      <Container>
+        <h1>Shopping List</h1>
+        <ul>
+          {location.state.ingredients.map((ingredient) => (
+            <li>
+              <Input type="checkbox" aria-label="checkbix for ingredient" />
+              {ingredient}
+            </li>
+          ))}
+        </ul>
+      </Container>
     </>
   );
   // TODO: Render a shopping list as a checklist. When the user clicks done shopping the checked items get added to the fridge.
 };
 
 export default ShoppingList;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: solid black 1px;
+  padding: 5px;
+`;
