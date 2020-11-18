8c7aaf09114f7771acafcad0592d90809428623a
dscription <dscriptionplayground@gmail.com>
2020-11-18 18:07:02 -0500

    add and remove newFridgeItem from state

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -1,55 +1,74 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import { Input, Button } from 'reactstrap';
 import styled from 'styled-components';
 
-const ShoppingList = ({ location }) => {
-  const { state } = location;
-  const { ingredients } = state;
-  const { currentIngredients } = state;
-  console.log(currentIngredients);
+class ShoppingList extends Component {
+  state = { newFridgeItems: [] };
 
-  // TODO: If ingredient is already in fridge, mark item as checked
-  // TODO: If use checks and item add it to state object.
+  handleCheckInput = (e) => {
+    // e.preventDefault();
+    const newFridgeItem = e.target.value;
+
+    if (this.state.newFridgeItems.includes(newFridgeItem)) {
+      const newFridgeItems = [...this.state.newFridgeItems];
+      newFridgeItems.splice(
+        this.state.newFridgeItems.indexOf(newFridgeItem),
+        1
+      );
+      this.setState({ newFridgeItems });
+    } else {
+      const newFridgeItems = [...this.state.newFridgeItems];
+      newFridgeItems.push(newFridgeItem);
+      this.setState({ newFridgeItems });
+    }
+  };
 
   // TODO: Once use clicks done shopping all items in state object will be added to the users fridge.
-  const handleDoneShopping = () => {
+  handleDoneShopping = () => {
     console.log('done');
     // Call a fridgeService function that will take all the items from state and pass them to the controller function to create fridge items.
   };
 
-  return (
-    <>
-      <Container>
-        <h1>Shopping List</h1>
-        <ul>
-          {ingredients.map((ingredient) =>
-            currentIngredients.includes(ingredient) ? (
-              <li>
-                <Input
-                  type="checkbox"
-                  aria-label={`checkbox input for $(ingredient)`}
-                  disabled
-                />
-                {ingredient}
-              </li>
-            ) : (
-              <li>
-                <Input
-                  type="checkbox"
-                  aria-label={`checkbox input for $(ingredient)`}
-                />
-                {ingredient}
-              </li>
-            )
-          )}
-        </ul>
-        <Button onClick={handleDoneShopping} color="success">
-          Done Shopping
-        </Button>
-      </Container>
-    </>
-  );
-};
+  render() {
+    const { state } = this.props.location;
+    const { ingredients } = state;
+    const { currentIngredients } = state;
+    return (
+      <>
+        <Container>
+          <h1>Shopping List</h1>
+          <ul>
+            {ingredients.map((ingredient) =>
+              currentIngredients.includes(ingredient) ? (
+                <li>
+                  <Input
+                    type="checkbox"
+                    aria-label={`checkbox input for $(ingredient)`}
+                    disabled
+                  />
+                  {ingredient}
+                </li>
+              ) : (
+                <li>
+                  <Input
+                    type="checkbox"
+                    aria-label={`checkbox input for $(ingredient)`}
+                    value={ingredient}
+                    onChange={this.handleCheckInput}
+                  />
+                  {ingredient}
+                </li>
+              )
+            )}
+          </ul>
+          <Button onClick={this.handleDoneShopping} color="success">
+            Done Shopping
+          </Button>
+        </Container>
+      </>
+    );
+  }
+}
 
 export default ShoppingList;
 
