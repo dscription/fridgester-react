58210af8c95f5ebdf45846de945ef5aa6a5017e1
dscription <dscriptionplayground@gmail.com>
2020-12-15 19:58:03 -0500

    remove useState hook from ShoppingList

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { Input, Button } from 'reactstrap';
 import styled from 'styled-components';
 
