ef872ff20913720ba88140fe4b2996565274d355
dscription <dscriptionplayground@gmail.com>
2020-12-15 19:58:49 -0500

    clean up Results

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import RecipeResult from '../../components/RecipeResult/RecipeResult';
 
