590227a44cc95a432aa4233ab8ccb5588d3aeb7b
dscription <dscriptionplayground@gmail.com>
2020-12-15 19:59:24 -0500

    more cleanup

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import FridgeItem from '../../components/FridgeItem/FridgeItem';
-import ResultItem from '../../components/FridgeItem/FridgeItem'
 
 const Favorites = (props) => {
   // will receive props and render recipes with the favorited tag.
