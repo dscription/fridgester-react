fbc847314a960b3350bf97c1b8943edeb4305fd6
dscription <dscriptionplayground@gmail.com>
2020-12-24 13:30:35 -0500

    add material-ui containter to app.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -12,7 +12,7 @@ import Landing from '../Landing/Landing';
 import Results from '../Results/Results';
 import './App.css';
 import * as recipeAPI from '../../services/recipeService';
-import {Container} from '../../components/styled_components'
+import {Container} from '@material-ui/core'
 
 
 class App extends Component {
